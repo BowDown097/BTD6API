03008c8e763370c61670af45391e704a484c0be6
Danny Parker <ben@webzone.com.au>
2020-11-12 19:08:54 +1030

    Update headers, retarget solution

--- Examples/TestDLL/main.cpp
+++ Examples/TestDLL/main.cpp
@@ -4,51 +4,86 @@
 #include "pch.hpp"
 #include <fstream>
 
+void makeHypersonic(GameModel* gmdl, const std::string& where) {
+
+    try {
+        size_t size = 0;
+        const Il2CppAssembly** assemblies = il2cpp_domain_get_assemblies(nullptr, &size);
+
+        const Il2CppAssembly* assembly = BTD6API::Assembly::get(assemblies, "Assembly-CSharp", size);
+
+        if (assembly == nullptr)
+        {
+            std::cout << "Error: Assembly-CSharp not found." << std::endl;
+            return;
+        }
+
+
+        Il2CppClass* ShoptowersClass = il2cpp_class_from_name(assembly->image, "Assets.Scripts.Unity.UI_New.InGame.RightMenu", "ShopMenu");
+        FieldInfo* shoptowersfield = il2cpp_class_get_field_from_name(ShoptowersClass, "instance");
+        const MethodInfo* _method12 = il2cpp_class_get_method_from_name(ShoptowersClass, "CreateTowerButton", 3);//TowerPurchaseButton *, ShopMenu_CreateTowerButton, (ShopMenu * __this, TowerModel * model, int32_t buttonIndex, bool showTowerCounts, MethodInfo * method));
+        TowerPurchaseButton* (*createbutton)(ShopMenu*, TowerModel*, int32_t, bool, MethodInfo*) = (TowerPurchaseButton * (*)(ShopMenu*, TowerModel*, int32_t, bool, MethodInfo*)) _method12->methodPointer;
+        ShopMenu* sm = 0;
+        il2cpp_field_static_get_value(shoptowersfield, &sm);
+
+        std::cout << "Current button count: " << sm->fields.buttonCount << std::endl;
+
+        sm->fields.buttonCount++;
+        TowerPurchaseButton* eee = createbutton(sm, (TowerModel*)gmdl->fields.towerSet->vector[0], sm->fields.buttonCount - 1, false, (MethodInfo*)_method12);
+    }
+    catch (Il2CppExceptionWrapper e) {
+        std::wcout << L"Il2cppException occurred:" << (wchar_t*)(e.ex->message->chars) << std::endl;
+    }
+
+
+}
+
+
 using namespace app;
 // Injected code entry point
 void Run()
 {
-	AllocConsole();
-	freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
+    AllocConsole();
+    freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
 
-	std::cout << "Initializing..." << std::endl;
+    Il2CppThread* thread = il2cpp_thread_attach(il2cpp_domain_get());
 
-	size_t size = 0;
-	const Il2CppAssembly** assemblies = il2cpp_domain_get_assemblies(nullptr, &size);
+    std::cout << "Initializing..." << std::endl;
 
-	const Il2CppAssembly* assembly = BTD6API::Assembly::get(assemblies, "Assembly-CSharp", size);
+    size_t size = 0;
+    const Il2CppAssembly** assemblies = il2cpp_domain_get_assemblies(nullptr, &size);
 
-	if (assembly == nullptr)
-	{
-		std::cout << "Error: Assembly-CSharp not found." << std::endl;
-		return;
-	}
+    const Il2CppAssembly* assembly = BTD6API::Assembly::get(assemblies, "Assembly-CSharp", size);
 
-	Il2CppClass* gameClass = il2cpp_class_from_name(assembly->image, "Assets.Scripts.Unity", "Game");
-	FieldInfo* towers = il2cpp_class_get_field_from_name(gameClass, "towers");
-	String__Array* towersArry = NULL;
-	il2cpp_field_static_get_value(towers, &towersArry);
-	String** towersCArry = towersArry->vector;
+    if (assembly == nullptr)
+    {
+        std::cout << "Error: Assembly-CSharp not found." << std::endl;
+        return;
+    }
 
-	if (towersArry == NULL)
-	{
-		std::cout << "Some error occurred when trying to access the game model." << std::endl;
-		return;
-	}
+    // do in-game patches (will need to patch InGame if user is currently in-game, just patching Game will do nothing in that case)
+    Il2CppClass* inGameClass = il2cpp_class_from_name(assembly->image, "Assets.Scripts.Unity.UI_New.InGame", "InGame");
+    FieldInfo* inGameInstanceInfo = il2cpp_class_get_field_from_name(inGameClass, "instance");
+    InGame* inGameInstAddr = 0;
+    il2cpp_field_static_get_value(inGameInstanceInfo, &inGameInstAddr);
 
-	for (auto i = 0; i < towersArry->max_length; ++i) {
-		std::wcout << BTD6API::StringUtils::toWideString(towersArry->vector[i]) << std::endl;
-	}
+    if (inGameInstAddr != NULL)
+    {
+        InGame* inGameInstance = (InGame*)inGameInstAddr;
+        makeHypersonic(inGameInstance->fields.bridge->fields.simulation->fields.model, "in-game");
+    }
+    // game patches
+    Il2CppClass* gameClass = il2cpp_class_from_name(assembly->image, "Assets.Scripts.Unity", "Game");
+    FieldInfo* gameInstanceInfo = il2cpp_class_get_field_from_name(gameClass, "instance");
+    Game* gameInstAddr = 0;
+    il2cpp_field_static_get_value(gameInstanceInfo, &gameInstAddr);
 
-	std::cout << "\n\n" << std::endl;
+    if (gameInstAddr == NULL)
+    {
+        std::cout << "Some error occurred when trying to access the game model." << std::endl;
+        return;
+    }
 
-	BTD6API::Array::resize(towersArry, towersArry->max_length + 1);
-
-
-	for (auto i = 0; i < towersArry->max_length; ++i) {
-		std::wcout << BTD6API::StringUtils::toWideString(towersArry->vector[i]) << std::endl;
-	}
-
-	std::wcout << L"Done!" << std::endl;
-
+    Game* gameInstance = (Game*)gameInstAddr;
+    makeHypersonic(gameInstance->fields.model, "game");
 }
